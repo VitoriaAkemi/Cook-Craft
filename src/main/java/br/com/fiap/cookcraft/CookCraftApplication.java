092d732a7d2637f45065b526b08c3af127c1ec52
gabpesouza <gabriel.psouza1997@outlook.com>
2023-11-11 15:29:22 -0300

    first

---
+++ src/main/java/br/com/fiap/cookcraft/CookCraftApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cookcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookCraftApplication.class, args);
+	}
+
+}
