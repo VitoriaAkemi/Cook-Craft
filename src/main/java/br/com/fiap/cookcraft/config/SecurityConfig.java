30f478ed3dc21dd21bdcd94f0f25752193452d71
gabpesouza <gabriel.psouza1997@outlook.com>
2023-11-20 22:44:17 -0300

    Correção get profiles

--- src/main/java/br/com/fiap/cookcraft/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/cookcraft/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "api/v1/users").permitAll()
                         .requestMatchers(HttpMethod.POST, "api/v1/auth").permitAll()
                         .requestMatchers(HttpMethod.POST, "api/v1/profiles").permitAll()
+                        .requestMatchers(HttpMethod.GET, "api/v1/profiles").permitAll()
                         .anyRequest().authenticated()
                 ).sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
